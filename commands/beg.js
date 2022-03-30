9ddc62e363154522aacb183327d41c9dd9095dfc
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-03-30 23:54:24 +0200

    Update beg.js
    updated to discord.js V13

--- commands/beg.js
+++ commands/beg.js
@@ -1,19 +1,80 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const profileModel = require('../models/profileSchema');
+
+    
+
 module.exports = {
-    name: 'beg',
-    cooldown: 20,
-    discription: "this is a beg command!",
-    async execute(client, message, args, Discord, cmd, profileData ){
-        const randomNumber = Math.floor(Math.random() * 50) + 1;
-        const response = await profileModel.findOneAndUpdate({
-            userID: message.author.id,           
-        }, 
-        {
-            $inc: {
-                coins: randomNumber,
-            },
+	data: new SlashCommandBuilder()
+		.setName('beg')
+		.setDescription('beg for money!'),
+	async execute(interaction) {
+        let profileData;
+        //start random number thingy
+    try {
+        profileData = await profileModel.findOne({ userID: interaction.member.id });
+        if (!profileData) {
+            let profile = await profileModel.create({
+                userID: interaction.member.id,
+                serverID: interaction.guild.id,
+                coins: 1000,
+                bank: 0,
+                robbing: 1,
+            });
+            profile.save();
         }
-    );
-      return message.channel.send(`${message.author.username}, great job you asked sayori for ${randomNumber}, coins`)
-  },
+    }catch(err) {
+        console.log(err);
+    }
+    const randomNumber = Math.floor(Math.random() * 50) + 1;
+    const response = await profileModel.findOneAndUpdate({
+        userID: interaction.member.id           
+    }, 
+    {
+        $inc: {
+            coins: randomNumber,
+        },
+    }
+);
+//end random number thingy  
+const people = [
+    "Will smith",
+    "SmallishBeans",
+    "Grian",
+    "PewDiePie",
+    "Your mom",
+    "Your dad",
+    "Sayori bot, wait what how she's dead. Anyway you asked her",
+    "Sayori",
+    "Poulpo",
+    "A racoon",
+    "A homeless man",
+    "Your sister in law"
+];
+const peoples = Math.floor(Math.random() * people.length);
+//end random ppl thingy
+
+const real = Math.floor(Math.random() * 40) +1;
+
+if(real < "20"){
+    const response = await profileModel.findOneAndUpdate({
+        userID: interaction.member.id           
+    }, 
+    {
+        $inc: {
+            coins: -randomNumber,
+        },
+    }
+);
+    return interaction.reply(`${interaction.member}, You asked ${people[peoples]} but, They said "ask your brother"`)
+    
+}else{
+
+if(randomNumber == "1" ){
+    return interaction.reply(`${interaction.member}, Great job you asked ${people[peoples]} for ${randomNumber} Coin`);
+}else{
+
+  return interaction.reply(`${interaction.member}, Great job you asked ${people[peoples]} for ${randomNumber} Coins`);
+}
+}
+	},
 };

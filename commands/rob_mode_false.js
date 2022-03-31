a2bf6412cee43687a98d6217772123dc16d81788
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-03-31 22:07:33 +0200

    Update rob_mode_false.js
    all updates to the code will be to discord.js V13 using /commands and a basic command handler

--- commands/rob_mode_false.js
+++ commands/rob_mode_false.js
@@ -1,25 +1,44 @@
-const profileModel = require('../models/profileSchema');
-module.exports = {
-    name: 'rob_mode_false',
-    discription: "this is a tomato command!",
-    async execute(client, message, args, Discord, cmd, profileData){
-        const amount = 1
-        if (profileData.robbing > 0) { 
-            await profileModel.findOneAndUpdate(
-                {
-                userID: message.author.id
-                },
-                {
-                $inc: {
-                robbing: -amount,
-                }
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const profileModel = require("../models/profileSchema");
+const amount = 1
 
-            }
-        );
-        return message.channel.send("done!~")
-        } else {
-            message.channel.send("it's already off!~")
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rob_false')
+		.setDescription('turns off robbing'),
+	async execute(interaction){
+        let profileData;
+    try {
+        profileData = await profileModel.findOne({ userID: interaction.member.id });
+        if (!profileData) {
+            let profile = await profileModel.create({
+                userID: interaction.member.id,
+                serverID: interaction.guild.id,
+                coins: 1000,
+                bank: 0,
+                robbing: 1,
+            });
+            profile.save();
         }
+    }catch(err) {
+        console.log(err);
+    }
+if (profileData.robbing > 0) { 
+    await profileModel.findOneAndUpdate(        {
+        userID: interaction.member.id
+        },
+        {
+        $inc: {
+        robbing: -amount,
+        }
+
+    }
+);
+return interaction.reply("done!")
+} else {
+    interaction.reply("it's already off")
+}
 
     }
 }

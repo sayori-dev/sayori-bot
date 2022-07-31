f0e1c300f537df12281bc8d952d31d9591062f65
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:20:31 +0200

    Delete key.js

--- commands/key.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'key',
-    discription: "this is a key command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('you found a secret!');
-
-    }
-}

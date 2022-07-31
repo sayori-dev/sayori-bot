46868154bacd476506d610aaa803d68659edef7b
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:20:42 +0200

    Delete mc.js

--- commands/mc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'mc',
-    discription: "this is a dab command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('i-i mean hes cute....');
-
-    }
-}

9446919c23781d3bc452022ef6ed72dc112c7f3b
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:22:10 +0200

    Delete sans.js

--- commands/sans.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'sans',
-    discription: "this is a sans command!",
-    execute(client, message, args, Discord){
-        message.channel.send('https://cdn.discordapp.com/attachments/805470960970367006/805601913940475914/859bb8834cebb298345ff3c29abe5229.jpg');
-
-    }
-}

364570d25126fa9c6160c13cacd91d7a36a6461e
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:18:01 +0200

    Delete soup.js

--- commands/soup.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'soup',
-    discription: "this is a soup command!",
-    execute(client, message, args, Discord){
-        message.channel.send(':canned_food:');
-
-    }
-}

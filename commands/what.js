7cd2305d10ff8b9cc2313e3d14e3bb8ca3c2f9ac
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:18:46 +0200

    Delete what.js

--- commands/what.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'what',
-    discription: "this is a fucking command!",
-    execute(client, message, args, Discord){
-        message.channel.send('https://cdn.discordapp.com/attachments/805470960970367006/822591725612302336/Screenshot_20210319-230342_YouTube.jpg')
-    }
-}

78568bf15f5ecac3189966dbaf280ade518534fa
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:23:07 +0200

    Delete hang.js

--- commands/hang.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'hang',
-    discription: "this is a hang command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('YOU FOUND A SECRET! ||https://tenor.com/view/hang-suizide-desgosto-suicide-rope-gif-15405316||');
-
-    }
-}

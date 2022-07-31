5143088644ade508d0c71f19ddfd00f96d2d2b09
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:22:19 +0200

    Delete scream.js

--- commands/scream.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'scream',
-    discription: "this is a scream command!",
-    execute(client, message, args, Discord){
-        message.channel.send('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-
-    }
-}

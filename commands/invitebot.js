5a460c27f9eb8b2b8013d872510b42f2f04eba51
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:20:25 +0200

    Delete invitebot.js

--- commands/invitebot.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'invitebot',
-    discription: "this is a invitebot command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('https://discord.com/oauth2/authorize?client_id=805433648517873664&scope=bot&permissions=142350');
-
-    }
-}

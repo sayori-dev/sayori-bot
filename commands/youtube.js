24302d1c9e789f309801f39f15aeea38b3093ea6
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:16:31 +0200

    Delete youtube.js

--- commands/youtube.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    discription: "this is a youtube command!",
-    execute(client, message, args, Discord){
-        message.channel.send('getting in contact with someone!');
-
-    }
-}

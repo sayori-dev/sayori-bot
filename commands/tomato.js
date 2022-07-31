9a270a5e87d8c89e2b55ddf9620672218f6e0f80
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:19:10 +0200

    Delete tomato.js

--- commands/tomato.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'tomato',
-    discription: "this is a tomato command!",
-    execute(client, message, args, Discord){
-        message.channel.send(':tomato:');
-
-    }
-}

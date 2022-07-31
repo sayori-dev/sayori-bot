e32faa3f8c0664e93769deebff62b6ba56628e1e
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:21:54 +0200

    Delete natsuki.js

--- commands/natsuki.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'natsuki',
-    discription: "this is a dab command!",
-    execute(client, message, args, Discord ){
-        message.channel.send("she's nice sometimes mean though...");
-
-    }
-}

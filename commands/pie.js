059a2c2b4ee283dfcab9ffdb40b67c96bbb39273
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:21:58 +0200

    Delete pie.js

--- commands/pie.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'pie',
-    discription: "this is a pie command!",
-    execute(client, message, args, Discord){
-        message.channel.send('3.1415926535897932384623');
-
-    }
-}

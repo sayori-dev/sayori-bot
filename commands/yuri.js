15b05329b3aaf1544548b4c6c906c496cab41ad1
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:16:23 +0200

    Delete yuri.js

--- commands/yuri.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'yuri',
-    discription: "this is a dab command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('damn.... those tits....');
-
-    }
-}

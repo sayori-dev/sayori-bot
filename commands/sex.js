3f48f74f55de8c765997d61dd418aa81b8197341
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:22:31 +0200

    Delete sex.js

--- commands/sex.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'sex',
-    discription: "this is a fucking command!",
-    execute(client, message, args, Discord){
-        message.channel.send(':flushed:')
-    }
-}

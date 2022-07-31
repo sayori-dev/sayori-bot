cff5de1c98a2d992ef0e2357019c971438d72f8e
sayori-dev <84012320+sayori-dev@users.noreply.github.com>
2022-07-31 02:20:16 +0200

    Delete invite.js

--- commands/invite.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'invite',
-    discription: "this is a invite command!",
-    execute(client, message, args, Discord ){
-        message.channel.send('hi, wanna meet the creator?, https://discord.gg/4kQYKDK232');
-
-    }
-}
